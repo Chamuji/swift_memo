d561e9cb373d45890d1eb1ca6d0c5fe75659c41d
chamuji <s1412052@s.do-johodai.ac.jp>
2022-10-17 16:47:31 +0900

    Initial Commit

---
+++ memo/memoApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoApp.swift
+//  memo
+//
+//  Created by 鈴木健太 on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct memoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
